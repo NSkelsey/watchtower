516531d0b72da28abf050b93190755ad5ae86c60
Ubuntu <nskelsey@gmail.com>
2014-06-20 14:12:08 +0000

    saving example schema

---
+++ setup/schema.py
@@ -0,0 +1,32 @@
+from sqlalchemy import (Column, String, Binary, create_engine, Enum, Boolean, Integer)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+path = "sqlite:///dat.db"
+engine = create_engine(path, echo=True)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+
+class Tx(Base):
+    __tablename__ = "transactions"
+
+    txid     = Column(String, primary_key=True)
+    block    = Column(String)
+    raw      = Column(Binary)
+
+class Block(Base):
+    __tablename__ = "blocks"
+
+    hash     = Column(String, primary_key=True)
+    prevhash = Column(String)
+
+class Enum(Base):
+    __tablename__ = "enmuyasd"
+
+    id      = Column(String, primary_key=True)
+    enum    = Column(Enum("blah", "foo"))
+    spent      = Column(Boolean)
+    value   = Column(Integer)
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
